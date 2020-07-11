bf5a98312f5b8120734ad13a90cbcb4b097796a1
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-07-11 12:26:35 +0200

    Updates

---
+++ precache-manifest.50258dedd86d9429401729902b887628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c6b3f4350c435d3b3fbfad037e76c8",
+    "url": "/currency_converter-react/index.html"
+  },
+  {
+    "revision": "640dd18ee4f07f46f78b",
+    "url": "/currency_converter-react/static/css/main.a5660c18.chunk.css"
+  },
+  {
+    "revision": "8ffcc9eb6284a8de611e",
+    "url": "/currency_converter-react/static/js/2.7b768caa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency_converter-react/static/js/2.7b768caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640dd18ee4f07f46f78b",
+    "url": "/currency_converter-react/static/js/main.456eb85d.chunk.js"
+  },
+  {
+    "revision": "13a1c52327a8d031e99c",
+    "url": "/currency_converter-react/static/js/runtime-main.a9a94a27.js"
+  }
+]);
