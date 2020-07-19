445430d7182f85379b1b1b05189b8738048b585e
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-07-19 14:51:49 +0200

    added calculateResult function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
-import React from "react";
+/* eslint-disable default-case */
+import React, { useState } from "react";
 import Form from "./Form";
 import Header from "./Header";
 import FormContainer from "./FormContainer";
 import Main from "./Main";
 import Result from "./Result";
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState("");
+  const calculateResult = (amount, firstCurrencyName, secondCurrencyName) => {
+    let plnValue = "";
+    let expectedAmount = "";
+    const rates = {
+      usd: 3.8922,
+      eur: 4.399,
+    };
+
+    const { usd, eur } = rates;
+
+    switch (firstCurrencyName) {
+      case "PLN":
+        plnValue = +amount;
+        break;
+
+      case "USD":
+        plnValue = amount * usd;
+        break;
+      case "EUR":
+        plnValue = amount * eur;
+        break;
+    }
+
+    switch (secondCurrencyName) {
+      case "PLN":
+        expectedAmount = plnValue;
+        break;
+      case "USD":
+        expectedAmount = plnValue / usd;
+        break;
+      case "EUR":
+        expectedAmount = plnValue / eur;
+        break;
+    }
+    setResult(expectedAmount);
+  };
   return (
-    <Main>
-      <Header />
-      <FormContainer>
-        <Form />
-        <Result result={2} />
-      </FormContainer>
-    </Main>
+    <>
+      <Main>
+        <Header />
+        <FormContainer>
+          <Form calculateResult={calculateResult} />
+          <Result result={result} />
+        </FormContainer>
+      </Main>
+    </>
   );
-}
+};
 
 export default App;
