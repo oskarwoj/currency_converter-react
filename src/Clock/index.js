db8f3c91e99c833381c953bfeae16efa54c4ef85
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-08-06 10:37:38 +0200

    changes after code review

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 
+const defaultDate = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+};
+
 const Clock = () => {
-  const [dateTime, setDateTime] = useState();
+  const [dateTime, setDateTime] = useState(
+    new Date().toLocaleDateString(undefined, defaultDate)
+  );
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const dateTemplate = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-      };
-      const newDate = new Date().toLocaleDateString("en-US", dateTemplate);
+      const newDate = new Date().toLocaleDateString(undefined, defaultDate);
       setDateTime(newDate);
     }, 1000);
+
     return () => {
       clearInterval(intervalId);
     };
